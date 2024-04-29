cbc7294f7bdf7588dc4d2dcd756f4ba4d8f14728
akshatgg <akshatg9636@gmail.com>
2024-04-30 00:19:17 +0545

    everything is ready except calendar

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const staff= document.getElementById("staff");
 const vendor=document.getElementById("vendor");
-
+const calendar =document.getElementById("calendar")
 const Reimbursement=()=>{
     vendor.style.display='none';
     staff.style.display='block';
+    calendar.style.display='none';
 
 }
 
@@ -11,24 +12,53 @@ const Reimbursement=()=>{
 const Payment=()=>{
     staff.style.display='none';
     vendor.style.display='block';
-
+    calendar.style.display='none';
 }
+
+
+const date = () => {
+    staff.style.display = 'none';
+    vendor.style.display = 'none';
+    calendar.style.display = 'block';
+    
+    const smallBoxes = document.querySelectorAll('.small-box');
+    smallBoxes.forEach(box => {
+        box.style.paddingBottom = '100%';
+    });
+}
+
 
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
     vendor.style.display = 'none';
+    calendar.style.display='none';
+
 });
 
 
 
 
 
-
-
-
-
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebars = document.querySelectorAll('.sidebar');
+    sidebars.forEach(sidebar => {
+      sidebar.addEventListener('click', function() {
+        clearActiveSidebar();
+        this.classList.add('active');
+      });
+    });
+  });
+  
+  function clearActiveSidebar() {
+    const sidebars = document.querySelectorAll('.sidebar');
+    sidebars.forEach(sidebar => {
+      sidebar.classList.remove('active');
+    });
+  }
+  
+  
 
 
 
